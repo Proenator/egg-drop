742d29d6b204074799754c97015f98a28106f9ab
seamus-hunter <72629167+seamus-hunter@users.noreply.github.com>
2020-10-11 16:43:05 -0400

    initial-commit

---
+++ Egg Drop/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float RotationSpeed; //Adds Rotation Speed property in Unity editor
+
+    void FixedUpdate() //Use void FixedUpdate whenever Physics are used
+    {
+        if (Input.GetButton("Horizontal")) //If the left or right key is held down
+            transform.Rotate(0, 0, RotationSpeed); //Rotate the player (Rotation Speed) degrees in the direction held down
+    }
+
+
+}
